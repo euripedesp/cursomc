846fc55acd968ac289e4f1a77ee81f17c04b1d65
euripedesp <comercial@horteladesign.com.br>
2018-09-14 14:44:37 -0300

    criando operação de instanciação

--- src/main/java/com/euripedes/cursomc/CursomcApplication.java
+++ src/main/java/com/euripedes/cursomc/CursomcApplication.java
@@ -1,12 +1,36 @@
 package com.euripedes.cursomc;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.euripedes.cursomc.domain.Categoria;
+import com.euripedes.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired //instancia automático
+	private CategoriaRepository categoriaRepository; //dependencia
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Categoria cat1 = new Categoria (null, "informatica");
+		Categoria cat2 = new Categoria (null, "Escritório");
+		
+		categoriaRepository.save(Arrays.asList(cat1, cat2));//chamando o objeto 
+		
+		
+	}
 }
+//executar a instanciação no memento que iniciar
